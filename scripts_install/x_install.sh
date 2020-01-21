75c236284ab98c8732afd3f9e864e5eba2f72301
Michael Bugert <git@mbugert.de>
2020-01-21 21:47:51 +0100

    Add more titlebar icons and wallpaper

--- scripts_install/x_install.sh
+++ scripts_install/x_install.sh
@@ -6,6 +6,7 @@ sudo apt update
 install compton
 install guake
 install redshift-gtk
+install feh
 install ulauncher
 # manually add extensions to ulauncher:
 # https://github.com/ulauncher/ulauncher-timer
