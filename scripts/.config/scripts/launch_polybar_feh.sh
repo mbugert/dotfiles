fc027280ca4c9b5f85f499c6e8ba7464212b2c18
Michael Bugert <git@mbugert.de>
2021-01-04 17:59:46 +0100

    Fix wallpaper issue when using multiple monitors

--- scripts/.config/scripts/launch_polybar_feh.sh
+++ scripts/.config/scripts/launch_polybar_feh.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Terminate already running bar instances
+# Terminate already running polybar instances
 killall -q polybar
 
 # Wait until the processes have been shut down
@@ -10,3 +10,7 @@ while pgrep -x polybar >/dev/null; do sleep 1; done
 for m in $(polybar --list-monitors | cut -d":" -f1); do
     MONITOR=$m polybar --reload mybar &
 done
+
+# Run feh with same random image on all screens, see https://www.reddit.com/r/i3wm/comments/6580vm/how_to_set_multiple_monitors_to_use_the_same/dg8fcqh/
+WALL=$(find "${HOME}/Pictures/Wallpapers/" -type f | sort -R | tail -1)
+feh --no-fehbg --bg-fill "$WALL"
