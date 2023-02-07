436cbdc0711c5f72aa109f8a8c8b3cb60194f0cb
Michael Bugert <git@mbugert.de>
2023-02-07 20:53:17 +0100

    Update lowest brightness for other laptop

--- ansible/roles/dotfiles/files/device_specific/f93/lvds_screens.py
+++ ansible/roles/dotfiles/files/device_specific/f93/lvds_screens.py
@@ -3,6 +3,6 @@ import pathlib
 # finding min_brightness requires trial & error, like this:
 # `echo 30 > /sys/class/backlight/intel_backlight/brightness`
 LVDS_SCREENS = [{"backlight_location": pathlib.Path("/sys/class/backlight/intel_backlight"),
-                 "min_brightness": 30,
+                 "min_brightness": 28,
                  "min_nits": 1,
                  "max_nits": 300}]
