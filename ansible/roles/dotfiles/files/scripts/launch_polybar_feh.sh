aafc9abf4b5de1c626ec35f6e4bc4e3ab68e1f05
Michael Bugert <git@mbugert.de>
2023-06-21 16:33:55 +0200

    Add script to enable aggressive powersaving

--- ansible/roles/dotfiles/files/scripts/launch_polybar_feh.sh
+++ ansible/roles/dotfiles/files/scripts/launch_polybar_feh.sh
@@ -6,9 +6,16 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
+# use status of cpu1 as a canary for whether we are powersaving
+cpu1_online=$(cat /sys/devices/system/cpu/cpu1/online)
+
 # Launch polybar on each monitor - https://github.com/polybar/polybar/issues/763#issuecomment-392960721
 for m in $(polybar --list-monitors | cut -d":" -f1); do
-    MONITOR=$m polybar --reload mybar &
+    if [[ $cpu1_online == "1" ]]; then
+        MONITOR=$m polybar --reload defaultbar &
+    else
+        MONITOR=$m polybar --reload lowpowerbar &
+    fi
 done
 
 # Run feh with same random image on all screens, see https://www.reddit.com/r/i3wm/comments/6580vm/how_to_set_multiple_monitors_to_use_the_same/dg8fcqh/
