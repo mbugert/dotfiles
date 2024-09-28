385e6f6bb31001bdd8d648d474c3848255b3d54a
Michael Bugert <git@mbugert.de>
2024-09-28 10:24:48 +0200

    Fix screen brightness (amdgpu_bl0 -> amdgpu_bl1)

--- ansible/roles/dotfiles/files/device_specific/45d/lvds_screens.py
+++ ansible/roles/dotfiles/files/device_specific/45d/lvds_screens.py
@@ -1,6 +1,6 @@
 import pathlib
 
-LVDS_SCREENS = [{"backlight_location": pathlib.Path("/sys/class/backlight/amdgpu_bl0"),
+LVDS_SCREENS = [{"backlight_location": pathlib.Path("/sys/class/backlight/amdgpu_bl1"),
                  "min_brightness": 1,
                  "min_nits": 1,
                  "max_nits": 500}]
